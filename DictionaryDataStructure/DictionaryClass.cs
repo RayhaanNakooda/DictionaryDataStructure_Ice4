1400a27b5910ca512bf681db085c0d97d92fb1c7
Rayhaan <rayhaannak1@gmail.com>
2021-10-18 12:05:50 +0200

    first commit

---
+++ DictionaryDataStructure/DictionaryClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDataStructure
+{
+    public class DictionaryClass
+    {
+
+        public static Dictionary<string, object> dict;
+        public static void Add(string strKey, object dataType)
+        {
+            if (!dict.ContainsKey(strKey))
+            {
+                dict.Add(strKey, dataType);
+            }
+            else
+            {
+                dict[strKey] = dataType;
+            }
+        }
+
+        public static T GetAnyValue<T>(string strKey)
+        {
+            object obj;
+            T retType;
+            dict.TryGetValue(strKey, out obj);
+
+            try
+            {
+                retType = (T)obj;
+            }
+            catch
+            {
+                retType = default(T);
+            }
+            return retType;
+
+        }
+    }
+}
---
+++ DictionaryDataStructure/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDataStructure
+{
+    class Program
+    {
+
+        DictionaryClass dict = new DictionaryClass();
+
+        static void Main(string[] args)
+        {
+            DictionaryClass.dict = new Dictionary<string, object>();
+
+            DictionaryClass.dict.Add("pie", 3.4123);
+            DictionaryClass.dict.Add("Apple Tart", "340 calories");
+            DictionaryClass.dict.Add("Poltry", "Fried chickens");
+            DictionaryClass.dict.Add("i", 7);
+
+            Console.WriteLine("pi = " + DictionaryClass.GetAnyValue<double>("pi"));
+            Console.WriteLine("Apple Tart = " + DictionaryClass.GetAnyValue<string>("Apple Tart"));
+            Console.WriteLine("Poltry = " + DictionaryClass.GetAnyValue<string>("Poltry"));
+            Console.WriteLine("i = " + DictionaryClass.GetAnyValue<int>("i"));
+            Console.ReadLine();
+        }
+    }
+}
